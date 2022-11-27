80cc98d2c4d1bd2954470cd8844dcf6ea0efd9c6
DhanushDivakar <93920313+DhanushDivakar@users.noreply.github.com>
2022-11-27 20:31:37 +0530

    Todo

--- lib/main.dart
+++ lib/main.dart
@@ -1,116 +1,26 @@
-import 'dart:convert';
-import 'dart:html';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:bloc/bloc.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutterbloc/models/task.dart';
+import 'package:flutterbloc/screens/task_screen.dart';
+
+import 'blocs/bloc_exports.dart';
 
 void main() {
-  runApp(const MyApp());
+  BlocOverrides.runZoned(
+    () => runApp(const MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false ,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-@immutable
-abstract class LoadAction {
-  const LoadAction();
-}
-
-@immutable
-class LoadPersonAction implements LoadAction {
-  final PersonUrl url;
-
-  const LoadPersonAction(this.url);
-}
-
-enum PersonUrl {
-  person1,
-  person2,
-}
-
-extension UrlString on PersonUrl {
-  String get urlString {
-    switch (this) {
-      case PersonUrl.person1:
-        return 'http://127.0.0.1:5500/api/person1.json';
-      case PersonUrl.person2:
-        return 'http://127.0.0.1:5500/api/person2.json';
-    }
-  }
-}
-
-@immutable
-class Person {
-  final String name;
-  final int age;
-
-  const Person({required this.name, required this.age});
-
-  Person.fromJson(Map<String, dynamic> json)
-      : name = json['name'] as String,
-        age = json['age'] as int;
-}
-
-Future<Iterable<Person>> getPersons(String url) => HttpClient()
-    .getUrl(Uri.parse(url))
-    .then((req) => req.close())
-    .then((resp) => resp.transform(utf8.decoder).join())
-    .then((str) => json.decode(str) as List<dynamic>)
-    .then((list) => list.map((e) => Person.fromJson(e)));
-
-@immutable
-class FetchResult {
-  final Iterable<Person> persons;
-  final bool isRetrievedFromCache;
-  const FetchResult({
-    required this.persons,
-    required this.isRetrievedFromCache,
-  });
-
-  @override
-  String toString() =>
-      'FetchResult (isRetrievedFromCache = $isRetrievedFromCache, persons = $persons)';
-}
-//flutter 
-class PersonsBloc extends Bloc<LoadAction, FetchResult?>{
-  final Map<PersonUrl, Iterable<Person>> _cache = {};
-   PersonsBloc(): super(null) {
-    on<LoadPersonAction>(
-      (event, emit) {
-        final url = event.url;
-        if(_cache.containsKey(url)){
-         final cachedPersons = _cache[url]!;
-        } 
-
-      }
-    );
-   }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Flutter Bloc"),
+    return BlocProvider(
+      create: (context) => TasksBloc()..add(AddTask(task: Task(title: 'Task1'))),
+      child: MaterialApp(
+        title: 'Flutter Tasks App',
+        theme: ThemeData(primarySwatch: Colors.indigo),
+        home: TasksScreen(),
       ),
     );
   }
---
+++ lib/screens/task_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import '../blocs/bloc_exports.dart';
+import '../models/task.dart';
+import '../widgets/tasks_list.dart';
+
+class TasksScreen extends StatelessWidget {
+  TasksScreen({super.key});
+
+  TextEditingController titleController = TextEditingController();
+
+  void _addTask(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) => SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(
+                    bottom: MediaQuery.of(context).viewInsets.bottom),
+                child: Column(children: [
+                  TextField(
+                    controller: titleController,
+                  ),
+                ]),
+              ),
+            ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<TasksBloc, TasksState>(
+      builder: (context, state) {
+        List<Task> taskList = state.allTasks;
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text("Tasks App"),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.add),
+              ),
+            ],
+          ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Center(
+                child: Chip(
+                  label: Text('Tasks:'),
+                ),
+              ),
+              TasksList(taskList: taskList)
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            tooltip: 'Add Task',
+            child: const Icon(Icons.add),
+          ),
+        );
+      },
+    );
+  }
+}
